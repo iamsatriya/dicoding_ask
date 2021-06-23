436aa14d8398ecab8860bb8b5b7fc787ee68b95f
Satriya <satriya.wicaksana888@gmail.com>
2021-06-23 18:22:29 +0700

    adding deeplink and action popup

--- app/src/main/java/com/satriyawicaksana888/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/satriyawicaksana888/mynavigation/DetailCategoryFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.satriyawicaksana888.mynavigation.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
     private var _binding: FragmentDetailCategoryBinding? = null
-    private val binding get () = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +23,12 @@ class DetailCategoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
-        val dataStock = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataStock = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock: $dataStock"
+        binding.btnProfile.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment))
     }
 
     override fun onDestroy() {
